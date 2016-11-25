5e0f718c8d9ddb783e08f995b96d259fced87da4
Tomasz Galecki <tomasz.galecki@hotmail.com>
2016-11-25 10:54:47 +0100

    [ADD] project's files

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <cstddef>
+#include <cstdlib>
+#include <string>
+
+#include "TreeMap.h"
+
+namespace {
+
+    template<typename K, typename V>
+    using Map = aisdi::TreeMap<K, V>;
+
+    void perfomTest() {
+        Map<int, std::string> map;
+        map[1] = "TODO";
+    }
+
+} // namespace
+
+int main(int argc, char** argv) {
+    const std::size_t repeatCount = argc > 1 ? std::atoll(argv[1]) : 10000;
+    for (std::size_t i = 0; i < repeatCount; ++i)
+        perfomTest();
+    return 0;
+}
